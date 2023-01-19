8b2bd49d84799ed56988bd05832c512721b92a29
Cloud-Hosting <arimonox22@gmail.com>
2023-01-19 20:33:33 +0700

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
-echo "========================"
-echo        "update"
-echo "========================"
+echo "=========update============= "
 sudo apt-get update > /dev/null 2>&1
-echo "========================"
-echo        "setup"
-echo "========================"
+echo           "done"
+echo "=========setup==============="
 sudo apt install --assume-yes --fix-broken > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo apt install xvfb -y > /dev/null 2>&1
@@ -16,12 +13,10 @@ sudo apt install libgbm1 -y > /dev/null 2>&1
 sudo apt install libgtk-3-0 -y > /dev/null 2>&1
 sudo apt install xfce4 -y > /dev/null 2>&1
 sudo apt install xfce4 xfce4-terminal -y > /dev/null 2>&1
-echo "========================"
-echo      "setup remote" 
-echo "========================"
+echo           "done"
+echo "=======setup remote===========" 
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb > /dev/null 2>&1
-echo "========================"
-echo      "complite" 
-echo "========================"
+echo           "done"
+
 
