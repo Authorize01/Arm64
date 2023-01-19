f6c1285e0df9dea9e28ad492db04fa14c2440d31
Cloud-Hosting <arimonox22@gmail.com>
2023-01-19 20:29:29 +0700

    update

--- setup.sh
+++ setup.sh
@@ -7,15 +7,15 @@ echo        "setup"
 echo "========================"
 sudo apt install --assume-yes --fix-broken > /dev/null 2>&1
 sudo apt install -y xrdp > /dev/null 2>&1
-sudo apt install -y xvfb > /dev/null 2>&1
-sudo apt install -y xserver-xorg-video-dummy > /dev/null 2>&1
-sudo apt install -y policykit-1 > /dev/null 2>&1
-sudo apt install -y xbase-clients /dev/null 2>&1
-sudo apt install -y python3-psutil > /dev/null 2>&1
-sudo apt install -y libgbm1 > /dev/null 2>&1
-sudo apt install -y libgtk-3-0 > /dev/null 2>&1
-sudo apt install -y xfce4 > /dev/null 2>&1
-sudo apt install -y xfce4 xfce4-terminal > /dev/null 2>&1
+sudo apt install xvfb -y > /dev/null 2>&1
+sudo apt install xserver-xorg-video-dummy -y > /dev/null 2>&1
+sudo apt install policykit-1 -y > /dev/null 2>&1
+sudo apt install xbase-clients -y /dev/null 2>&1
+sudo apt install python3-psutil -y > /dev/null 2>&1
+sudo apt install libgbm1 -y > /dev/null 2>&1
+sudo apt install libgtk-3-0 -y > /dev/null 2>&1
+sudo apt install xfce4 -y > /dev/null 2>&1
+sudo apt install xfce4 xfce4-terminal -y > /dev/null 2>&1
 echo "========================"
 echo      "setup remote" 
 echo "========================"
